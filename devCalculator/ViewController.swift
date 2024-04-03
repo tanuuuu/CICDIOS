f69b8705bc8e328bf9a7a7bb2c6aea834466780a
CICDIOS <tanuu096@gmail.com>
2024-04-03 10:32:47 +0530

    share scheme

--- devCalculator/ViewController.swift
+++ devCalculator/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenter
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBOutlet weak var text3: UITextField!
+    
+    @IBOutlet weak var text4: UITextField!
+    
+    @IBOutlet weak var text5: UITextField!
+    
+    
+    @IBOutlet weak var incomelable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calBtn(_ sender: Any) {
+        AppCenter.start(withAppSecret: "dcb25d33-632d-4c7b-88d3-e7ba45b1b3ef", services:[
+          Crashes.self
+        ])
+        
+    }
+    
 }
 
