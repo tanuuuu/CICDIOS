c2d649f9db25dfc99285533288d89c7e75c8b54f
CICDIOS <tanuu096@gmail.com>
2024-04-03 11:02:45 +0530

    share scheme4

--- devCalculator/ViewController.swift
+++ devCalculator/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("rahul")
     }
 
     @IBAction func calBtn(_ sender: Any) {
